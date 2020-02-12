0d74f6643fcb9865963117e071743ae2dac6d363
tmm99 <55119374+tmm99@users.noreply.github.com>
2020-02-12 08:11:51 +0800

    '疫情补充'

--- src/components/Area.tsx
+++ src/components/Area.tsx
@@ -5,14 +5,11 @@ const Area = (props: {areaTree:AreaType []})=>{
 
     console.log('area...', props.areaTree);
     return <>
-        <div>
-            <h3>国内病例</h3>
-        </div>
-        <div>
-            <h3>海外国家</h3>
-        </div>
-    </>
+     {/* 数据 */}
+   </>
 }
+
+export default Area
 
 // export default Area
 // <div>
